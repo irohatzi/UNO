d31228b910635be06c0d115d39bb6dbeee1ceb30
Valentina Krammer <v.krammer@codebridge.at>
2020-12-14 20:41:05 +0100

    impl genCards; changed styleClasses

--- main.js
+++ main.js
@@ -4,21 +4,17 @@
 let gameId;
 let playersCards = [];
 
-let card = {
-    col: '',
-    val: '',
-    sco: '',
-    text: '',
-};
+let card = {};
 let currentPlayer;
-// let topCard;
-// let cardArr;
+
 
 let wildColor = "";
-let colorRC ;
-let valueRC ;
+let colorRC;
+let valueRC;
 
 let img;
+let topCard;
+let cardArr;
 
 
 // modaler dialog funzt, damit erspart man sich das permanente eingeben:
@@ -33,8 +29,11 @@ let player2 = 'p2';
 let player3 = 'p3';
 let player4 = 'p4';
 
+
+
+
 players.push(player1, player2, player3, player4);
-// // Modalen Dialog öffnen um Namen einzugeben
+//! Modalen Dialog öffnen um Namen einzugeben
 // $('#playerNames').modal()
 
 // document.getElementById('playerNamesForm').addEventListener('submit', function (evt) {
@@ -93,7 +92,7 @@ document.getElementById('p2').innerText = player2;
 document.getElementById('p3').innerText = player3;
 document.getElementById('p4').innerText = player4;
 
-// die klammern gehören auch zum modalen dialog 
+//! die klammern gehören auch zum modalen dialog 
 //      }
 
 // });
@@ -103,7 +102,7 @@ playersCards.push(document.getElementById("p2cards").id);
 playersCards.push(document.getElementById("p3cards").id);
 playersCards.push(document.getElementById("p4cards").id);
 
-// unbedingt für mod dialog wieder einkommentieren!
+//! unbedingt für mod dialog wieder einkommentieren!
 // $('#playerNames').modal('hide');
 // post();
 
@@ -143,40 +142,42 @@ async function post() {
 
 
         gameId = result.Id;
-     //   console.log(gameId);
+        //   console.log(gameId);
 
+        // im response von post ist der nächste Player der erste zu spielende
         currentPlayer = result.NextPlayer;
+        cardArr = result.Players[0].Cards;
+        console.log(cardArr);
 
         console.log(currentPlayer);
 
-        let firstCard = document.getElementById("decks");
-        firstCard.setAttribute("id", "topCard");
-        img = new Image();
-        img.src = "cards/" + result.TopCard.Color + result.TopCard.Value + ".png";
-        img.height = 161;
-        firstCard.appendChild(img);
-        firstCard = result.TopCard;
-
-        if(result.TopCard.Text == "Draw2"){
-            drawCard();
-            drawCard();
-        }
+        let center = document.getElementById("decks");
+        //  console.log(result.TopCard.Color);
+        card = result.TopCard;
+        let pic = generateCard(card);
+        pic.setAttribute("id", "topCard");
+        center.appendChild(pic);
 
 
-        // playerCards
+        // if(result.TopCard.Text == "Draw2"){
+        //     drawCard();
+        //     drawCard();
+        // }
+
+
+
+        // //* playerCards
         for (let i = 0; i < playersCards.length; i++) {
 
             for (let j = 0; j < result.Players[i].Cards.length; j++) {
-                let cardDiv = document.createElement("div");
-                cardDiv.setAttribute("id", i + "card2play" + j);
-                cardDiv.setAttribute("onclick", "replyId(this.id)");
-                cardDiv.setAttribute("class", "cards");
-                let img = new Image();
-                img.src = "cards/" + result.Players[i].Cards[j].Color + result.Players[i].Cards[j].Value + ".png";
-                img.height = 100;
-                //            cardDiv.addEventListener("click", removeCard());
-                cardDiv.appendChild(img);
-                document.getElementById(playersCards[i]).appendChild(cardDiv);
+
+                card = result.Players[i].Cards[j];
+                img = generateCard(card);
+                img.setAttribute("id", i + "card2play" + j);
+                img.setAttribute("onclick", "replyId(this.id)");
+                img.setAttribute("class", "cards");
+
+                document.getElementById(playersCards[i]).appendChild(img);
             }
         }
 
@@ -189,13 +190,13 @@ async function post() {
         for (let i = 0; i < scores.length; i++) {
             let idString = i + 1;
             document.getElementById("score" + idString);
-            //     console.log("TEST FÜR SCORE: " + "score" + idString);
+            console.log("TEST FÜR SCORE: " + "score" + idString);
             // für Score evtl ein div im index.html einbauen, mir is das zu stressig zum aufrufen
             //   let p = document.createElement("p");
             //   p.innerText = "Score";
             let score = document.createElement("p");
             score.innerText = result.Players[i].Score;
-            score.setAttribute("class", "scoreStyle");
+            score.setAttribute("class", "score");
 
             document.getElementById(scores[i]).appendChild(score);
         }
@@ -203,23 +204,45 @@ async function post() {
         //  getTopCard();
         // drawCard();
 
-        const btnDraw = document.getElementById('drawDeck');
-        btnDraw.addEventListener("click", drawCard);
 
-       cardArr = getCards();
-       topCard = getTopCard();
+
+        //    cardArr = getCards();
+        //    topCard = getTopCard();
     }
     else {
         alert("HTTP-Error: " + response.status)
     }
 }
+const btnDraw = document.getElementById('drawDeck');
+btnDraw.addEventListener("click", drawCard);
 
-// wenn modaler dialog auskommentiert! - da post aufrufen!
+
+
+//! wenn modaler dialog auskommentiert! - da post aufrufen!
 post();
-let topCard;
-let cardArr;
 
+function game() {
 
+    getTopCard();
+    // wenn +2 oder +4 aufliegt, kartenhand neu generieren vom aktuellen spieler
+    //klick auf play oder drawCard
+    //
+
+    playCard(clickedId);
+    // solange der nächste spieler noch karten hat, ruf game auf
+}
+
+function generateCard(card) {
+    console.log(card);
+    console.log(card.Color);
+
+    let pic = document.createElement("img");
+    let color = card.Color;
+    let value = card.Value;
+    pic.src = "cards/" + color + value + ".png";
+    return pic;
+
+}
 
 async function getTopCard() {
 
@@ -234,18 +257,18 @@ async function getTopCard() {
     if (response.ok) {
         let result = await response.json();
         console.log(result);
-      //  alert(JSON.stringify(result))
-      if(topCard.Text == "Draw2" || topCard.Text == "Draw4"){
-        let cardArrSize = cardArr.length;
-        let helper = topCard.text.charAt(length-1);
-        console.log(helper);
-        while((cardArrSize + helper) > cardArr.length){
-            drawCard();
+        //  alert(JSON.stringify(result))
+        if (topCard.Text == "Draw2" || topCard.Text == "Draw4") {
+            let cardArrSize = cardArr.length;
+            let helper = topCard.text.charAt(length - 1);
+            console.log(helper);
+            while ((cardArrSize + helper) > cardArr.length) {
+                generateCards();
+            }
         }
-      }
-  
-      
-      // TOPCARD value aus result rausspeichern - DAS FUNKTIONIERT -NICHT LÖSCHEN!!!!!!!!
+
+
+        // TOPCARD value aus result rausspeichern - DAS FUNKTIONIERT -NICHT LÖSCHEN!!!!!!!!
         topCard = result;
         // return topCard;
 
@@ -283,21 +306,24 @@ async function drawCard() {
         cardDiv.setAttribute("onclick", "replyId(this.id)");
         cardDiv.setAttribute("class", "cards");
 
-        img = new Image();
-        img.src = "cards/" + result.Card.Color + result.Card.Value + ".png";
-        img.height = 100;
-        // 420Test ob vl das div attribute haben kann
-        cardDiv.val = result.Card.Value;
-        cardDiv.col = result.Card.Color;
-        cardDiv.appendChild(img);
-        // Hilfsvariable um auf richtigen index zuzugreifen
+
+        card = result.Card;
+        img = generateCard(card);
+        img.setAttribute("id", i + "card2play" + j);
+        img.setAttribute("onclick", "replyId(this.id)");
+        img.setAttribute("class", "cards");
+
+        document.getElementById(playersCards[i]).appendChild(img);
+
+
+        // Hilfsvariable um auf richtigen player zuzugreifen (index vom array ≙ +1 von id -->weil ich die ids ab p1 und nicht p0 vergeben habe)
 
         check += 1;
         console.log("p" + check + "cards");
-        document.getElementById("p" + check + "cards").appendChild(cardDiv);
+        document.getElementById("p" + check + "cards").appendChild(img);
 
         //   console.log(result.Card.Score);
-      //  console.log(document.getElementById("score" + check).innerText);
+        //  console.log(document.getElementById("score" + check).innerText);
         let newScore = document.getElementById("score" + check).innerText;
         //     console.log(newScore);
 
@@ -309,8 +335,8 @@ async function drawCard() {
 
         //   console.log(result.NextPlayer);
         // das sollte auch nach dem ablegen einer karte gemacht werden!
-      //  currentPlayer = result.NextPlayer;
-      //  getCards();
+        //  currentPlayer = result.NextPlayer;
+        //  getCards();
 
         //  }
     }
@@ -333,24 +359,25 @@ async function getCards() {
 
     if (response.ok) {
         let result = await response.json();
-     //   console.log(result.Cards[2].Value);
-
-    
-     console.log(result);
-
-      // CardArr values aus result rausspeichern - DAS FUNKTIONIERT -NICHT LÖSCHEN!!!!!!!!
-     cardArr = result.Cards;       
+        //   console.log(result.Cards[2].Value);
 
 
-            // for(let i=0; i<result.Cards.length;i++){
-            //     cardArr[i] =  {card: {
-            //         val : result.Cards[i].Value,
-            //         col : result.Cards[i].Color,
-            //         sco : result.Cards[i].Score
-            //     }
-            //         };
-            //     }
-            
+        //     console.log(result);
+
+        // CardArr values aus result rausspeichern - DAS FUNKTIONIERT -NICHT LÖSCHEN!!!!!!!!
+        cardArr = result.Cards;
+        // !!!!!!CHECK OB IM RESULTARRAY MEHR KARTEN ALS IM SPIELFELD(WEGEN +4 / +2)      
+
+
+        // for(let i=0; i<result.Cards.length;i++){
+        //     cardArr[i] =  {card: {
+        //         val : result.Cards[i].Value,
+        //         col : result.Cards[i].Color,
+        //         sco : result.Cards[i].Score
+        //     }
+        //         };
+        //     }
+
         //    console.log(card);
 
         // try later:
@@ -374,8 +401,8 @@ function replyId(clickedId) {
 
 
 
-   // let anotherTest = document.getElementById(clickedId);
-   // console.log(anotherTest.getAttribute(val));
+    // let anotherTest = document.getElementById(clickedId);
+    // console.log(anotherTest.getAttribute(val));
 
 
     // check ob von richtiger Kartenhand gespielt:
@@ -390,37 +417,36 @@ function cardCheck(clickedId) {
     // variable für richtiger player check
     let i2CheckPlayer = players.indexOf(currentPlayer);
     // indexNummer die an der ID angehängt ist rauslesen:
-    let i4CardArr = clickedId.charAt(clickedId.length-1);
+    let i4CardArr = clickedId.charAt(clickedId.length - 1);
+
     console.log(i4CardArr);
     console.log(i2CheckPlayer);
     if (i2CheckPlayer == clickedId.charAt(0)) {
-        // alert(clickedId);
-         //  getCards();
+        alert(clickedId);
+        //  getCards();
         //console.log(cardArr.pop());
         let removeCard = cardArr.splice(i4CardArr, 1);
         colorRC = removeCard[0].Color;
         valueRC = removeCard[0].Value;
-        console.log(colorRC);
+        //      console.log(colorRC);
 
-        console.log(cardArr);
+        //     console.log(cardArr);
         //check ob karte gespielt werden darf:
         // let cardX = Object.assign({},cardArr[i4CardArr]);
         let colorTC = topCard.Color;
         let valueTC = topCard.Value;
 
         if (colorRC == "Black" || valueRC == valueTC || colorRC == colorTC) {
-          //  alert("PlayCard kann implementiert werden!");
+            alert("Valid CarD!");
 
-               
-           
-            playCard(clickedId);
+            //      playCard(clickedId);
         }
     } else {
         alert("Falsche Kartenhand!");
-        if(removeCard === defined){
+        if (removeCard === defined) {
             cardArr.push(removeCard);
             // check if removeCard in cardArr
-            console.log(cardArr);
+            //         console.log(cardArr);
         }
 
     }
@@ -429,8 +455,8 @@ function cardCheck(clickedId) {
 
 async function playCard(clickedId) {
 
-    let response = await fetch( "https://nowaunoweb.azurewebsites.net/api/game/playCard/" + gameId +
-    "?value=" + valueRC + "&color=" + colorRC + "&wildColor=" + wildColor, {
+    let response = await fetch("https://nowaunoweb.azurewebsites.net/api/game/playCard/" + gameId +
+        "?value=" + valueRC + "&color=" + colorRC + "&wildColor=" + wildColor, {
         method: "PUT",
         contentType: "application/json",
         headers: {
@@ -440,38 +466,38 @@ async function playCard(clickedId) {
 
     if (response.ok) {
         let result = await response.json();
-        console.log(result);
-     //   alert(JSON.stringify(result));
+        //  console.log(result);
+        //   alert(JSON.stringify(result));
 
-     if(colorRC == "Black"){
-        $('#WCForm').modal()
-   }
+        if (colorRC == "Black") {
+            $('#WCForm').modal()
+        }
 
         let deleteCard = document.getElementById(clickedId);
-    //    console.log(clickedId);
+        //    console.log(clickedId);
         deleteCard.parentElement.removeChild(deleteCard);
         clickedId = "";
 
         let newTC = document.getElementById("topCard").childNodes;
         console.log(newTC);
 
-    //    document.getElementById("topCard").removeChild(img);
+        //    document.getElementById("topCard").removeChild(img);
 
-    
+
         let imgNew = new Image();
         imgNew.src = "cards/" + colorRC + valueRC + ".png";
-        imgNew.height = 161;
+        // imgNew.height = 161;
 
 
         document.getElementById("topCard").replaceChild(imgNew, img);
         img = imgNew;
 
-        console.log(result.Player);
+        //   console.log(result.Player);
 
         currentPlayer = result.Player;
-        console.log(currentPlayer);
-        getCards();
-        
+        //  console.log(currentPlayer);
+        // getCards();
+
     }
     else {
         alert("HTTP-Error: " + response.status)
